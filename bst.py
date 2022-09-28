ee793b327417eb7fcc173d74fc900799d66b699f
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-09-28 11:17:05 +0100

    feat: now at bst

---
+++ bst.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while(True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                
+
+
+
+
+my_tree = BinarySearchTree()
+
+print(my_tree.root)
