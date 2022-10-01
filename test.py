06951bee7a4b5325761608b1948b13c13be2a2ed
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-10-01 23:04:52 +0100

    graphy

--- test.py
+++ test.py
@@ -1,18 +1,23 @@
-arr = [1,-3,5,0,-4,3]
-# count_less = 0
-# count_grt = 0
-# count_zero = 0
-# n = len(arr)
-# for x in range(len(arr)):
-#     if arr[x] < 0:
-#         count_less += 1
-#     elif arr[x] > 0:
-#           count_less += 1
-#     else:
-#         count_zero += 1
-# grt = round(count_grt/n, 6)
-# less = round(count_less/n, 6)
-# zero = round(count_zero/n, 6)
-# print(grt, '\n',less, '\n', zero)
-for x in arr:
-    print(x)
+# [1,3,5]
+# [2,4,5]
+
+# def item_in_common(list1, list2):
+#     for i in list1:
+#         for j in list2:
+#             if i ==j:
+#                 return True
+#     return False
+
+def item_in_common(list1, list2):
+    my_dict = {}
+    for i in list1:
+        my_dict[i] = True
+
+    for j in list2:
+        if j in my_dict:
+            return True
+    return False
+
+list1 = [1,3,5]
+list2 = [2,4,6]
+print(item_in_common(list1, list2))
